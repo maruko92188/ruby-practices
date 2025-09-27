528e3f8052f8eb17b65f5da907d3aa9e8812aeea
maruko92188 <maruko.kazuki.f@gmail.com>
2025-09-27 20:58:10 +0900

    lsコマンドを作る1の課題提出

---
+++ 04.ls/ls.rb
@@ -0,0 +1,30 @@
+#! /usr/bin/env ruby
+# frozen_string_literal: true
+
+COLUMNS = 3
+COLUMNS_DISTANCE = 5
+
+def output(make_output_file_names, file_names)
+  longest_string = file_names.max_by(&:size)
+  string_width = longest_string.size + COLUMNS_DISTANCE
+  make_output_file_names.each do |row|
+    row.each do |file_name|
+      print file_name.to_s.ljust(string_width)
+    end
+    puts
+  end
+end
+
+def make_output_file_names
+  rows = file_names.size.ceildiv(COLUMNS)
+  additional_blanks = COLUMNS * rows - file_names.size
+  full_file_names = file_names + Array.new(additional_blanks)
+  sliced_file_names = full_file_names.each_slice(rows).to_a
+  sliced_file_names.transpose
+end
+
+def file_names
+  Dir['*'].sort_by(&:downcase)
+end
+
+output(make_output_file_names, file_names)
