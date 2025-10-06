97933538eeadce6fef3f91c8dfaa8cf6945108d2
maruko92188 <maruko.kazuki.f@gmail.com>
2025-10-06 23:23:27 +0900

    file_namesメソッドの結果を引数として渡すように修正

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -4,11 +4,11 @@
 COLUMNS = 3
 COLUMNS_DISTANCE = 5
 
-def main
+def main(file_names)
   return exit if file_names.empty?
   longest_file_name = file_names.max_by(&:size)
   width = longest_file_name.size + COLUMNS_DISTANCE
-  build_output_file_names.each do |row|
+  build_output_file_names(file_names).each do |row|
     row.each do |file_name|
       print file_name.to_s.ljust(width)
     end
@@ -16,11 +16,10 @@ def main
   end
 end
 
-def build_output_file_names
-  created_file_names = file_names
-  rows = created_file_names.size.ceildiv(COLUMNS)
-  additional_blanks = COLUMNS * rows - created_file_names.size
-  full_file_names = created_file_names + Array.new(additional_blanks)
+def build_output_file_names(file_names)
+  rows = file_names.size.ceildiv(COLUMNS)
+  additional_blanks = COLUMNS * rows - file_names.size
+  full_file_names = file_names + Array.new(additional_blanks)
   sliced_file_names = full_file_names.each_slice(rows).to_a
   sliced_file_names.transpose
 end
@@ -29,4 +28,4 @@ def file_names
   Dir['*'].sort_by(&:downcase)
 end
 
-main
+main(file_names)
