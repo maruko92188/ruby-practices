941a81c4ed11ad18ff760cacf156cb15990664d9
maruko92188 <maruko.kazuki.f@gmail.com>
2025-10-12 17:12:52 +0900

    定数名TAB_SPACEをTAB_WIDTHに変更

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,7 +2,7 @@
 # frozen_string_literal: true
 
 COLUMNS = 3
-TAB_SPACE = 8
+TAB_WIDTH = 8
 
 def main
   file_names = create_file_names
@@ -12,7 +12,7 @@ def main
   build_file_names_table(file_names).each do |row|
     last_columns_number = row.size
     row.each.with_index(1) do |file_name, index|
-      tab_count = longest_file_name.div(TAB_SPACE) + 1 - file_name.to_s.size.div(TAB_SPACE)
+      tab_count = longest_file_name.div(TAB_WIDTH) + 1 - file_name.to_s.size.div(TAB_WIDTH)
       print index == last_columns_number ? file_name : "#{file_name}#{"\t" * tab_count}"
     end
     puts
