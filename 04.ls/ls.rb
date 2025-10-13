70d2c777d8a79c98714d073113b632157c99da21
maruko92188 <maruko.kazuki.f@gmail.com>
2025-10-13 16:08:12 +0900

    処理の流れを変更
    ファイル名の取得後にタブ文字を加えるようにした
    この処理を行うメソッドadd_tab_characterを新たに定義
    メソッドの順を呼ばれる順に変更した

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,28 +8,30 @@ def main
   file_names = search_file_names
   return if file_names.empty?
 
-  column_width = calculate_column_width(file_names)
   build_file_names_table(file_names).each do |row|
-    last_columns_number = row.size
-    row.each.with_index(1) do |file_name, index|
-      tab_count = column_width - file_name.to_s.size.div(TAB_WIDTH)
-      print index == last_columns_number ? file_name : "#{file_name}#{"\t" * tab_count}"
-    end
-    puts
+    puts row.join.rstrip
+  end
+end
+
+def build_file_names_table(file_names)
+  rows = file_names.size.ceildiv(COLUMNS)
+  additional_blanks = COLUMNS * rows - file_names.size
+  full_file_names = add_tab_character(file_names) + Array.new(additional_blanks)
+  sliced_file_names = full_file_names.each_slice(rows).to_a
+  sliced_file_names.transpose
+end
+
+def add_tab_character(file_names)
+  column_width = calculate_column_width(file_names)
+  file_names.map do |file_name|
+    tab_count = column_width - file_name.size.div(TAB_WIDTH)
+    "#{file_name}#{"\t" * tab_count}"
   end
 end
 
 def calculate_column_width(file_names)
   longest_file_name = file_names.max_by(&:size).size
   longest_file_name.div(TAB_WIDTH) + 1
-end
-
-def build_file_names_table(file_names)
-  rows = file_names.size.ceildiv(COLUMNS)
-  additional_blanks = COLUMNS * rows - file_names.size
-  full_file_names = file_names + Array.new(additional_blanks)
-  sliced_file_names = full_file_names.each_slice(rows).to_a
-  sliced_file_names.transpose
 end
 
 def search_file_names
