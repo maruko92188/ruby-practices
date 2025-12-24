980f67e9201a213ec57ba2243f3ecd48896dee8d
maruko92188 <maruko.kazuki.f@gmail.com>
2025-12-25 00:49:26 +0900

    出力を共通化

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -7,9 +7,11 @@ WIDTH = 8
 
 def main
   options = parse_options
-  command_arguments = ARGV
-
-  command_arguments.empty? ? display_input_from_pipe(options) : display_input_from_command_line(command_arguments, options)
+  tergets = ARGV.empty? ? [$stdin] : ARGV
+  tergets.each do |terget|
+    content = collect_content(terget)
+    row_table = format_row_table(content_table)
+    display_row(row_table, options)
 end
 
 def parse_options
@@ -26,55 +28,28 @@ def parse_options
   options
 end
 
-def display_input_from_pipe(options)
-  contents = $stdin.read
-  lines = contents.count("\n").to_s.rjust(WIDTH)
-  words =  contents.split(' ').size.to_s.rjust(WIDTH)
-  byte = contents.size.to_s.rjust(WIDTH)
+def collect_content(terget)
+  content = ARGV.empty? ? terget.read : File.read(terget)
+  name = ARGV.empty? ? nil : "#{terget}"
+  { content:, name: }
+end
+
+def format_row_table(content_table)
+  {
+    lines: content_table[:content].count("\n").to_s.rjust(WIDTH),
+    words: content_table[:content].split(' ').size.to_s.rjust(WIDTH),
+    byte: content_table[:content].size.to_s.rjust(WIDTH),
+    name: " #{content_table[:name]}"
+  }
+end
+
+def display_row(row_table, options)
   row = []
-  row << lines if options[:lines]
-  row << words if options[:words]
-  row << byte if options[:byte]
+  row << row_table[:lines] if options[:lines]
+  row << row_table[:words] if options[:words]
+  row << row_table[:byte] if options[:byte]
+  row << row_table[:name]
   puts row.join
-end
-
-def display_input_from_command_line(command_arguments, options)
-  row_table = format_row_table(command_arguments)
-  row_table.each do |rows|
-    row = []
-    row << rows[:lines] if options[:lines]
-    row << rows[:words] if options[:words]
-    row << rows[:byte] if options[:byte]
-    row << rows[:file_name]
-    puts row.join
-  end
-  display_total(row_table, options) if command_arguments.size >= 2
-end
-
-def format_row_table(command_arguments)
-  file_names = command_arguments
-  file_names.map do |file_name|
-    contents = File.read(file_name)
-    {
-      lines: contents.count("\n").to_s.rjust(WIDTH),
-      words: contents.split(' ').size.to_s.rjust(WIDTH),
-      byte: contents.size.to_s.rjust(WIDTH),
-      file_name: " #{file_name}"
-    }
-  end
-end
-
-ef display_total(row_table, options)
-  total_table = %i[lines words byte].to_h do |key|
-    total_count = row_table.sum { |rows| rows[key].to_i}
-    [key, total_count.to_s.rjust(WIDTH)]
-  end
-  total_row = []
-  total_row << total_table[:lines] if options[:lines]
-  total_row << total_table[:words] if options[:words]
-  total_row << total_table[:byte] if options[:byte]
-  total_row << ' total'
-  puts total_row.join
 end
 
 main
